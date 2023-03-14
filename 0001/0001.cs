d08ad331dc1a2d83ad057d58130385f474fc11de
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-03-14 21:48:30 +0800

    0001 on Mar.14

---
+++ 0001/0001.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (dict.ContainsKey(target - nums[i])) { 
+                return new int[2] {dict[target - nums[i]], i};
+            }
+            else {
+                if (!dict.ContainsKey(nums[i])){
+                    dict.Add(nums[i], i);
+                }
+            }
+        }
+        return null;
+    }
+}
