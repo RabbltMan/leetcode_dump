16ac559e48adf30f6fe3e9a040b23ea6eb58c3cc
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-03-18 17:57:55 +0800

    1616 on Mar.18

--- 1616/1616.py
+++ 1616/1616.py
@@ -1,16 +1,9 @@
 class Solution:
-    def isPalindrome(self, subString):
-        for i in range(len(subString)):
-            if (subString[i] != subString[-i - 1]):
-                return False
-        return True
-
-    def singleCheck(self, a: str, b: str) -> bool:
-        aPointer, bPointer = 0, len(b) - 1
-        while (aPointer < bPointer and a[aPointer] == b[bPointer]):
-            aPointer += 1
-            bPointer -= 1
-        return self.isPalindrome(b[aPointer: bPointer + 1]) or self.isPalindrome(a[aPointer: bPointer + 1])
-
     def checkPalindromeFormation(self, a: str, b: str) -> bool:
-        return self.singleCheck(a, b) or self.singleCheck(b, a)
+        def singleCheck(a: str, b: str) -> bool:
+            aPointer, bPointer = 0, len(b) - 1
+            while (a[aPointer] == b[bPointer] and aPointer < bPointer):
+                aPointer += 1
+                bPointer -= 1
+            return b[aPointer: bPointer + 1] == b[aPointer: bPointer + 1][::-1] or a[aPointer: bPointer + 1] == a[aPointer: bPointer + 1][::-1]
+        return singleCheck(a, b) or singleCheck(b, a)
