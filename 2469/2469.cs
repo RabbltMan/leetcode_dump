452348ef86e7b615e6005a87183eae593338569f
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-03-21 10:16:11 +0800

    2469 on Mar.21

---
+++ 2469/2469.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public double[] ConvertTemperature(double celsius) {
+        double[] ans = new double[2] {celsius + 273.15, celsius * 1.80 + 32.00};
+        return ans;
+    }
+}
