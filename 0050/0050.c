e155386d4d039a8abb30610db72c12c3adc54cff
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-08-13 15:54:33 +0800

    0050 on Aug.13

---
+++ 0050/0050.c
@@ -0,0 +1,12 @@
+double quickPow(double x, long long N){
+    if (N == 0) {
+        return 1.0;
+    }
+    double y = quickPow(x, N/2);
+    return N % 2 == 0 ? y * y : y * y * x ;
+}
+
+double myPow(double x, int n){
+    long long N = n;
+    return N >= 0 ? quickPow(x, N) : 1 / quickPow(x, -N);
+}
